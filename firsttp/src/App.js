500f780d6f9efcba693d3898d8ff0e035f4f97e7
wambabriand <briandwamba@yahoo.fr>
2023-08-01 01:28:22 +0200

    modification done

---
+++ firsttp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./compoments/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ firsttp/src/compoments/Hello.js
@@ -0,0 +1,9 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hellokkkkkkkk</h1>
+    </div>
+  );
+}
+
+export default Hello;
